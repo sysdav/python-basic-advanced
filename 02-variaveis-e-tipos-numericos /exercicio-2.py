76b42f46bc33972108a350dfe9946af7f552f1ae
sysdav <contatodavidwilliams@gmail.com>
2025-05-13 08:18:32 -0300

    exercicios completos

---
+++ 02-variaveis-e-tipos-numericos /exercicio-2.py
@@ -0,0 +1,11 @@
+# 2. Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
+
+print("Escreva tres numeros inteiros.")
+numero = int(input("numero um: "))
+numero2 = int(input("numero dois: "))
+numero3 = int(input("numero tres: "))
+
+soma = numero + numero2 + numero3
+
+print(f"A soma dos numeros e {soma}")
+
---
+++ 02-variaveis-e-tipos-numericos /exercicio-3.py
@@ -0,0 +1,9 @@
+#3. Faça um programa que recebe três valores e apresente a soma dos quadrados dos valores lidos.
+
+valor1 = input("digite um numero ")
+valor2 = input("digite um numero ")
+valor3 = input("digite um numero ")
+
+soma = (int(valor1)**2)+(int(valor2)**2)+(int(valor3)**2)
+
+print(soma)
