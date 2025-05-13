de7ccab53966a2732ca569bca80be9fe5a024686
sysdav <contatodavidwilliams@gmail.com>
2025-05-13 06:08:09 -0300

    types booleans

---
+++ 02-variaveis-e-tipos-numericos /boolean.py
@@ -0,0 +1,34 @@
+# Tipo Booleano
+
+# So existe dois tipos
+# True e False : Sempre primeira letra maiuscula.
+
+ativo = False
+
+# Operacoes Basicas.
+# Uso do NOT
+# Faz a negacao de um valor booleano ou seja
+
+# not True = False
+# not False = True
+
+print(not ativo)
+
+# Uso do OR(OU) basta uma afirmacao para ser verdadeira.
+
+print(True or False, " #Uso do OR")
+print(False or True," #Uso do OR")
+print(False or False," #Uso do OR")
+
+# Uso do E(and) Todas as arfimacoes tem que ser verdadeiras.
+
+print(True and True, " #Uso do AND")
+print(False and True," #Uso do AND")
+print(False and False," #Uso do AND")
+
+# operadores de comparacao
+# >
+# <
+# >=
+# <=
+# !=
