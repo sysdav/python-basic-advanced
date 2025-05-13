dd82421e1785cea58317e0db49f56506f2dfd914
sysdav <contatodavidwilliams@gmail.com>
2025-05-13 04:29:31 -0300

    Entendo Fucionalidades do Dir e Help

---
+++ dir_help.py
@@ -0,0 +1,15 @@
+# Utilitarios Python para auxiliar na progamacao
+
+helloword = "helloword"
+number = 1
+
+
+# Dir -> Apresenta todos os metodos/funcoes e atributos e propriedades de um tipo de dado ou variavel
+
+dir(helloword.islower())
+
+# Help -> Apresenta a documentacao de como usar os metodos/funcoes e atributos e propriedades disponiveis.
+
+# help(variavel.metodo)
+
+print(helloword.islower()) # retorna true por que a pergunta e: e minuscula ?
