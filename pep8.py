e9de5d425c9357e64e13fa7f0e0292cee458618c
sysdav <contatodavidwilliams@gmail.com>
2025-05-13 04:05:25 -0300

    pep8 basics

--- pep8.py
+++ pep8.py
@@ -1,4 +1,51 @@
+# Comentario simples
 """
-PEP8 - Python enhancement proposal 8
+Comentario em bloco.
+"""
+# PEP8 - Python enhancement proposal 8
+# Python proposta de melhoria 8
 
-"""
+# algumas melhorias que podem ajudar no codigo.
+
+#-[1] Utilizar Camel Case para classes
+class Calculadora:
+    pass
+
+class Cientifica:
+    pass
+
+#-[2] Ao criar nomes de variaveis e funcoes sempre minusculo e separalas por udnerline.
+
+nome = "david"
+sobrenome = "william"
+nome_completo = nome + " " + sobrenome
+print(nome_completo)
+
+def soma():
+    pass
+
+def multiplicacao():
+    pass
+
+#-[3] Indentar o codigo com spaces.
+#-[4] Sempre dentro de uma class dar espaco de uma linha para cada funcao.
+#-[5] Imports
+
+#- Import errado.
+
+#import sys,os
+
+#- Import certo.
+
+#import os
+#import sys
+#import this
+
+# caso tenha muitos imports
+
+# from types import (
+#   StringType,
+#   ListType,
+#   SetType,
+#   AnotherType
+#   )
