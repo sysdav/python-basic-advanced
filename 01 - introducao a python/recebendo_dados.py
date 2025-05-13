e103fb8ba36026d525df9fba696078aeee0faccb
sysdav <contatodavidwilliams@gmail.com>
2025-05-13 04:55:33 -0300

    Criacao de pasta 01 - introducao a python
    tambem criei o arquivo recebendo dados para testar as funcionalidades do input() e print()
    fora o exemplo de saida de dados nas versoes 2x e 3x

---
+++ 01 - introducao a python/recebendo_dados.py
@@ -0,0 +1,20 @@
+print ("Seu nome: ")
+nome = input()
+
+print("Sua idade: ")
+idade = input()
+
+#entrada
+
+#processamento
+
+#saida
+
+# Exemplo de saida Python 2.x
+# print("Ola %s"% nome)
+
+# Exemplo de saida de dados 3.x
+# print('seja bem-vindo {0} voce tem {01} anos'.format(nome,idade))
+
+# Exemplo de saida de dados 3.7+
+print(f'seja bem-vindo {nome} voce tem {idade} anos')
