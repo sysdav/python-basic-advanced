1720acba2bce52e54e279ed73e51835f6ec291d5
sysdav <contatodavidwilliams@gmail.com>
2025-05-13 05:13:05 -0300

    criado pasta 02-variaveis e arquivo type-numero
    basicamente essa pasta ira abranger o basico sobre tipos de dados.

---
+++ 02-variaveis-e-tipos-numericos /type-number.py
@@ -0,0 +1,26 @@
+# O Tipo Numerico.
+
+# O tipo numerico em python recebe valores dos numeros reais.
+
+# Tipos de numericos
+# Float 2.5 , 3.14 ,3.200.00
+# Int 1,2,3,4,5,6,7,...
+# Complex 3 + 4j
+
+idade = "22"
+idade_convertida = int(idade)
+a = 2
+b = 4
+
+print(a + b)  # Soma
+print(a - b)  # Subtração
+print(a * b)  # Multiplicação
+print(a / b)  # Divisão (float)
+print(a // b) # Divisão inteira
+print(a % b)  # Resto da divisão
+print(a ** b) # Potência
+
+# Verificando tipo de dado
+print(type(a))         # int
+print(type(a / b))     # float
+print(type(idade_convertida))# int
